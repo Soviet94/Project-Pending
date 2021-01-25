ca90942de197a2219652bfa7a02683703f0cea57
Soviet94 <38730151+Soviet94@users.noreply.github.com>
2021-01-25 21:02:35 +0000

    Basic BART API setup

---
+++ src/BART_API.java
@@ -0,0 +1,61 @@
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class BART_API {
+
+	// Base of the API call
+	private String host = "http://api.bart.gov/api/bsa.aspx?";
+
+	// API Key
+	private String key = "&key=MW9S-E7SL-26DU-VV8V";
+	
+	
+	
+	//API call for the current departure estimates
+	public Document estimatedDeparture() {
+		return apiCall(host, key, "cmd=etd");
+	}
+	
+	// API call for the train count
+	public Integer trainCount() {
+		Document response = apiCall(host, key, "cmd=count");
+
+		if (response == null) {
+			return null;
+		}
+
+		response.getDocumentElement().normalize();
+		return Integer.parseInt((((Element) response.getElementsByTagName("root").item(0))
+				.getElementsByTagName("traincount").item(0).getTextContent()));
+	}
+
+	// Base API call function
+	private Document apiCall(String host, String key, String cmd) {
+		HttpURLConnection con;
+		try {
+			String call = host + cmd + key;
+			URL url = new URL(call);
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+
+			InputStream responseStream = con.getInputStream();
+
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			return builder.parse(responseStream);
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			return null;
+		}
+
+	}
+
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
+	
+	public static BART_API bart = new BART_API();
+	
     public static void main(String[] args) {
        System.out.println("Hello World!");
+       System.out.println(bart.trainCount());
     }
 }
