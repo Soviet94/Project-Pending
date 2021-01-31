0306282b4918f3c112b48c776d8476337a327169
Soviet94 <38730151+Soviet94@users.noreply.github.com>
2021-01-31 11:41:52 +0000

    Full list of stations from the API

--- src/BART_Station.java
+++ src/BART_Station.java
@@ -7,6 +7,9 @@ public class BART_Station {
 	private BART_Station north;
 	private BART_Station south;
 
+	public BART_Station() {
+	}
+
 	public BART_Station(String name, String abbr, int platforms, BART_Station north, BART_Station south) {
 		this.name = name;
 		this.abbr = abbr;
@@ -15,24 +18,44 @@ public class BART_Station {
 		this.south = south;
 	}
 
-	public BART_Station getNorthConnection() {
-		return north;
-	}
-	
-	public BART_Station getSouthConnection() {
-		return south;
-	}
-	
 	public String getName() {
 		return name;
 	}
-	
-	public String getAbbreviation() {
+
+	public String getAbbr() {
 		return abbr;
 	}
-	
+
 	public int getPlatforms() {
 		return platforms;
+	}
+
+	public BART_Station getNorth() {
+		return north;
+	}
+
+	public BART_Station getSouth() {
+		return south;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAbbr(String abbr) {
+		this.abbr = abbr;
+	}
+
+	public void setPlatforms(int platforms) {
+		this.platforms = platforms;
+	}
+
+	public void setNorth(BART_Station north) {
+		this.north = north;
+	}
+
+	public void setSouth(BART_Station south) {
+		this.south = south;
 	}
 
 }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 public class HelloWorld {
 	
 	public static BART_API bart = new BART_API();
@@ -5,5 +7,6 @@ public class HelloWorld {
     public static void main(String[] args) {
        System.out.println("Hello World!");
        System.out.println(bart.trainCount());
+       Map<String, BART_Station> list = bart.stationList();
     }
 }
