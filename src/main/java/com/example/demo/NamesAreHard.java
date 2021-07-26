51b79a62f62346c4390bf34e9228cbfed92521f3
Soviet94 <38730151+Soviet94@users.noreply.github.com>
2021-07-26 09:31:00 +0100

    Cleaning

--- src/main/java/com/example/demo/NamesAreHard.java
+++ src/main/java/com/example/demo/NamesAreHard.java
@@ -2,9 +2,6 @@ package com.example.demo;
 
 import java.util.Map;
 
-import com.example.demo.BART_API;
-import com.example.demo.BART_Station;
-
 public class NamesAreHard {
 	
 	public static BART_API bart = new BART_API();
